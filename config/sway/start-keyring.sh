105e06a1e676b6f7fb96385cb7a717f69b401316
naba-r <justwish7@gmail.com>
2026-04-05 12:38:09 +0300

    Update start-keyring.sh

--- config/sway/start-keyring.sh
+++ config/sway/start-keyring.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-# Initialize the keyring daemon (connect to the one PAM started or start a new one)
-# We eval the output to set the environment variables in this script's scope
-eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
+# Start (or replace) gnome-keyring and import the env vars it prints
+eval "$(/usr/bin/gnome-keyring-daemon --replace --start --components=pkcs11,secrets,ssh)"
 
-# Export these variables to the DBus user session and Systemd
-# This is the "Bridge" that lets Brave find the Keyring
+# Export for child processes + push into systemd/dbus activation env
 export SSH_AUTH_SOCK
 export GNOME_KEYRING_CONTROL
 
 dbus-update-activation-environment --systemd \
-    WAYLAND_DISPLAY \
-    XDG_CURRENT_DESKTOP=sway \
-    SSH_AUTH_SOCK \
-    GNOME_KEYRING_CONTROL \
-    DISPLAY
+  DISPLAY \
+  WAYLAND_DISPLAY \
+  XDG_CURRENT_DESKTOP=sway \
+  SSH_AUTH_SOCK \
+  GNOME_KEYRING_CONTROL
