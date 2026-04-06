1c892c045e018ed4ebf5f321e919fbe8938e2097
naba-r <justwish7@gmail.com>
2026-04-06 10:12:21 +0300

    Update start-keyring.sh

--- config/sway/start-keyring.sh
+++ config/sway/start-keyring.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
-# Start (or replace) gnome-keyring and import the env vars it prints
-eval "$(/usr/bin/gnome-keyring-daemon --replace --start --components=pkcs11,secrets,ssh)"
-
-# Export for child processes + push into systemd/dbus activation env
+eval "$(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)"
 export SSH_AUTH_SOCK
 export GNOME_KEYRING_CONTROL
-
 dbus-update-activation-environment --systemd \
   DISPLAY \
   WAYLAND_DISPLAY \
